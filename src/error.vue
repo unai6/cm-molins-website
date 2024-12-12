c0ca8ed500503f11e6d37c4e6532c1edcf459a23
Unai Gonzalez <ugonzalez@dogfydiet.com>
2024-12-12 20:53:54 +0100

    Style error page

--- src/error.vue
+++ src/error.vue
@@ -1,7 +1,28 @@
-<script setup></script>
+<script setup>
+const localePath = useLocalePath()
+</script>
 
 <template>
-  <div>
-    Página error
+  <div class="error">
+    <p class="error__message">{{ $t('error.label.pageNotFound') }}</p>
+    <NuxtLink :to="localePath('/')">
+      <img src="/images/fundacion-joaquin-molins-logo.jpg">
+    </NuxtLink>
   </div>
 </template>
+
+<style lang="scss">
+.error {
+  display: flex;
+  flex-direction: column;
+  gap: $spacer-double;
+  align-items: center;
+  justify-content: center;
+  height: 100dvh;
+  font-weight: $font-weight-bold;
+
+  &__message {
+    font-size: ms(3);
+  }
+}
+</style>
